de2bfdc2d3ad0511b25286f12ed160b09c68d93e
root <planetbridging@gmail.com>
2022-04-17 15:52:10 +1000

    testing hosting

--- bridgeware/back/index.js
+++ bridgeware/back/index.js
@@ -1,20 +1,28 @@
-const http = require("http");
-const https = require("https");
-var express = require("express");
-var cors = require("cors");
-var uuid = require("uuid"),
+const http = require('http')
+const https = require('https')
+var express = require('express')
+var cors = require('cors')
+var uuid = require('uuid'),
   app = express(),
-  server = require("http").createServer(app);
-const axios = require("axios").default;
+  server = require('http').createServer(app)
+const axios = require('axios').default
 
-const { encrypt, decrypt } = require("./helpers/crypto");
+const { encrypt, decrypt } = require('./helpers/crypto')
 
-(async () => {
-  console.log("welcome to the bridgeware");
-  var tk = uuid.v4();
+var port = 80
 
-  var secretKey = tk;
+;(async () => {
+  console.log('welcome to the bridgeware')
+  var tk = uuid.v4()
 
-  var txt_test = encrypt(secretKey, "Hello from socket shannon");
-  console.log(txt_test);
-})();
+  var secretKey = tk
+
+  var txt_test = encrypt(secretKey, 'Hello from socket shannon')
+  console.log(txt_test)
+})()
+
+app.use('/', express.static(__dirname + '/build/'))
+
+server.listen(port, function () {
+  console.log('listening on *:' + port)
+})
